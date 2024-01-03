85f12ad2907672bb633461e182e7d722c6a01283
amollgaikwad <amollaxman.g@gaiansolutions.com>
2024-01-03 17:00:57 +0530

    Added Structure Header Component

---
+++ src/components/Structure/Structure.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "./structure.module.css"
+const Structure = () => {
+  return (
+    <div className={style.container}>
+        
+      <div className="top"></div>
+      <div className="bottom"></div>
+    </div>
+  )
+}
+
+export default Structure
