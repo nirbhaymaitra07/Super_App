086d6c9f0e5937b89bf78ee399cfda4c854017d1
amollgaikwad <amollaxman.g@gaiansolutions.com>
2024-01-04 11:56:15 +0530

    added mapcomponent and header and headertab

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
-
+import style from "./header.module.css"
+import logo from "/images/Revvee 2.svg"
+import HeaderTab from '../HeaderTab/HeaderTab'
+import cloud from "/images/cloud.svg"
+import mike from "/images/mike.svg"
+import people from "/images/people.svg"
+import bin from "/images/bin.svg"
+import roadside from "/images/roadside.svg"
+import profile from "/images/Profile.svg"
 const Header = () => {
+  let tabsData=[
+    {
+      "tabImg":cloud,
+      "tabText":"37° F"
+    },
+    {
+      "tabImg":roadside,
+      "tabText":"0.5 mi. away"
+    },
+    {
+      "tabImg":bin,
+      "tabText":"2 mi. away"
+    },
+    {
+      "tabImg":mike,
+      "tabText":"2 mi. away"
+    },
+    {
+      "tabImg":people,
+      "tabText":"$10000"
+    },
+    
+  ]
   return (
-    <div>
-       
+    <div className={style.container}>
+       <div className={style.left}>
+        <img src={logo} className={style.logo} alt="" />
+       </div>
+       <div className={style.right}>
+        <div className={style.tabs_container}>
+          {tabsData.map((item)=><HeaderTab tab={item}/>)}
+        </div>
+        <div className={style.date_time_container}>
+          <div className={style.time}>
+            <h3>26 December | 11:00</h3>
+            <h6>Lower Manhattan, NYC</h6>
+          </div>
+          <div className={style.profile}>
+            <img src={profile} alt="profile" />
+          </div>
+        </div>
+       </div>
     </div>
   )
 }
--- src/components/Structure/Structure.jsx
+++ src/components/Structure/Structure.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import style from "./structure.module.css";
-import video from "../../../public/assests/video.mp4";
+import video from "/assests/video.mp4";
+import Header from "../Header/Header";
+import Happening from "../Happening/Happening";
 
 const Structure = () => {
   return (
@@ -9,14 +11,20 @@ const Structure = () => {
         <video className={style.video} src={video} autoPlay loop muted></video>
       </div>
       <div className={style.main_container}>
-        <div className={style.top}></div>
+        <div className={style.top}>
+            <Header/>
+        </div>
         <div className={style.headings}>
-            <div className={style.map_heading}></div>
+            <div className={style.map_heading}>
+              <h5 className={style.title}>Happening around you!</h5>
+            </div>
             <div className={style.advertise_heading}></div>
             <div className={style.apps_heading}></div>
         </div>
         <div className={style.bottom}>
-            <div className={style.map_component}></div>
+            <div className={style.map_component}>
+              <Happening/>
+            </div>
             <div className={style.advertise_component}></div>
             <div className={style.apps}></div>
         </div>
