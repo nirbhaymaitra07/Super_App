85b137eb75b5c6ac069f98697b6df63fb9a72e5e
Abhay Kamath <abhay.kamath@gaiansolutions.com>
2024-01-04 10:58:34 +0530

    Added the Happening Around You component

---
+++ src/data/happeningInfo.js
@@ -0,0 +1,37 @@
+const mias_info = [
+  {
+    mia_name: "amplyfund",
+    mia_message: {
+      count: 3,
+      message: "fundraisers near you",
+    },
+    gif: "/gifs/amplyfund_gif.gif",
+    logo: "/svgs/amplyfund_logo.svg",
+    gifDuration: 3000,
+    totalDuration: 10000,
+  },
+  {
+    mia_name: "clink",
+    mia_message: {
+      count: 2,
+      message: "issues reported by you",
+    },
+    gif: "/gifs/clink_gif.gif",
+    logo: "/svgs/clink_logo.svg",
+    gifDuration: 3000,
+    totalDuration: 10000,
+  },
+  {
+    mia_name: "herehear",
+    mia_message: {
+      count: 3,
+      message: "events near you",
+    },
+    gif: "/gifs/herehear_gif.gif",
+    logo: "/svgs/herehear_logo.png",
+    gifDuration: 3000,
+    totalDuration: 10000,
+  },
+];
+
+export default mias_info;
