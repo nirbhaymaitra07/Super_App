c39c7a8734f51e3b9952e02be213835d6a49cb3a
amollgaikwad <amollaxman.g@gaiansolutions.com>
2024-01-04 22:13:03 +0530

    added demo.js

---
+++ src/scripts/demo.js
@@ -0,0 +1,32 @@
+import {
+    wait,
+    simulateClick,
+    simulateHover,
+    simulateVerticalScroll,
+    simulateHorizontalScroll,
+    addClass,
+    removeClass,
+    simulateMouseIn,
+  } from "./helpers";
+
+
+  let headerTab="._tabIcon_1t4jx_33"
+  let advertise="._container_jer6r_1"
+  let app="._container_17mkm_1"
+ export async function runDemo(){//hoverclass _tabIcon_1t4jx_33 _container_jer6r_1   _container_17mkm_1
+    await wait(1000)
+
+    simulateHover(document.querySelectorAll(headerTab)[0],"hoverclass")
+    await wait(1000)
+    simulateHover(document.querySelectorAll(headerTab)[2],"hoverclass")
+    await wait(1000)
+    simulateHover(document.querySelectorAll(advertise)[0],"hoverclass")
+    await wait(1000)
+    simulateHover(document.querySelectorAll(advertise)[2],"hoverclass")
+    await wait(1000)
+    simulateHover(document.querySelectorAll(app)[0],"hoverclass")
+    simulateMouseIn(document.querySelectorAll(app)[0])
+    await wait(1000)
+    simulateHover(document.querySelectorAll(app)[3],"hoverclass")
+
+  }
--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,2 +1,81 @@
+import anime from "animejs";
 export const wait = (delay) =>
   new Promise((resolve) => setTimeout(resolve, delay));
+
+
+
+export const simulateClick = (element) => {
+  const event = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  });
+  element.dispatchEvent(event);
+};
+export const simulateMouseIn = (element) => {
+  const event = new MouseEvent("mouseenter", {
+    view: window,
+  });
+  element.dispatchEvent(event);
+};
+
+
+export const simulateHover = async (element, className) => {
+  element.classList.add(className);
+  await wait(1000);
+  element.classList.remove(className);
+};
+
+export const simulateHoverAndClick = async (element, className) => {
+  element.classList.add(className);
+  await wait(1000);
+  const event = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+  });
+  element.dispatchEvent(event);
+  element.classList.remove(className);
+};
+
+export const addClass = async (element, className) => {
+  element.classList.add(className);
+};
+
+export const removeClass = async (element, className) => {
+  element.classList.remove(className);
+};
+
+export const simulateVerticalScroll = async (
+  element,
+  distance,
+  duration = 1000
+) => {
+  return new Promise(async (resolve) => {
+    anime({
+      targets: element,
+      scrollTop: distance, // Adjust this value to control the scroll position
+      duration: duration, // Animation duration in milliseconds
+      easing: "easeInOutQuad", // Easing function
+    });
+    await wait(duration);
+    resolve();
+  });
+};
+
+export const simulateHorizontalScroll = async (
+  element,
+  distance,
+  duration = 1000
+) => {
+  return new Promise(async (resolve) => {
+    anime({
+      targets: element,
+      scrollLeft: distance, // Adjust this value to control the scroll position
+      duration: duration, // Animation duration in milliseconds
+      easing: "easeInOutQuad", // Easing function
+    });
+    await wait(duration);
+    resolve();
+  });
+};
