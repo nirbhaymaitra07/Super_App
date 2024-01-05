2f040184f46f833803299f6565b22a6f8b1d22ed
bharathGaianSolutions <bharath.kumar@gaiansolutions.com>
2024-01-05 12:17:01 +0530

    created revee main container

---
+++ src/components/REVEE/MainContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './mainContainer.module.css'
+
+function MainContainer() {
+  return (
+    <div className={`${styles.mainContainer}`}>
+             hello
+    </div>
+  )
+}
+
+export default MainContainer
